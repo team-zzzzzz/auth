49a49801d034a3ef034eb35cb9fabca1e7e7d01e
juno-choi <ililil9482@naver.com>
2024-04-29 15:52:46 +0900

    feat : Security 인증이 없을 경우 예외 처리

--- src/main/java/com/team5z/projectAuth/global/config/SecurityConfig.java
+++ src/main/java/com/team5z/projectAuth/global/config/SecurityConfig.java
@@ -1,8 +1,10 @@
 package com.team5z.projectAuth.global.config;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -11,17 +13,30 @@ import org.springframework.security.web.SecurityFilterChain;
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
+    private static final String[] DEFAULT_LIST = {
+            "/api/auth/swagger-ui/index.html"
+    };
+
+    private static final String[] WHITE_LIST = {
+            "/api/auth/**"
+    };
+
+    private static final String[] AUTH_LIST = {
+            "/api/auth/test"
+    };
 
     @Bean
-    public SecurityFilterChain filterChain(HttpSecurity security) throws Exception{
+    public SecurityFilterChain filterChain(HttpSecurity security, ObjectMapper objectMapper, Environment env) throws Exception{
         return security.csrf(c -> c.disable())
                 .cors(c -> c.disable())
                 .headers(c -> c.frameOptions(f -> f.disable()).disable())   // h2 설정
                 .authorizeHttpRequests(auth -> {
-                    auth.requestMatchers(PathRequest.toH2Console()).permitAll()
+                    auth
+                            .requestMatchers(AUTH_LIST).hasRole("SELLER")
+                            .requestMatchers(PathRequest.toH2Console()).permitAll()
                             .anyRequest().permitAll();
                 }).sessionManagement(s -> s.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-//                .exceptionHandling(e -> e.authenticationEntryPoint(new CustomAuthenticationEntryPoint())) // 인증 예외처리
+                .exceptionHandling(e -> e.authenticationEntryPoint(new MyEntryPoint(objectMapper, env))) // 인증 예외처리
 //                .apply()  // 인증처리
                 .build();
     }
