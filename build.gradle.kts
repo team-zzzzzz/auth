340ca3ad4678eb8995792a54c17047125dd83ce6
juno-choi <ililil9482@naver.com>
2024-05-01 16:53:33 +0900

    build : redis 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -42,6 +42,7 @@ dependencies {
 	compileOnly("io.jsonwebtoken:jjwt-api:0.11.2")
 	runtimeOnly("io.jsonwebtoken:jjwt-impl:0.11.2")
 	runtimeOnly("io.jsonwebtoken:jjwt-jackson:0.11.2")
+	implementation("org.springframework.boot:spring-boot-starter-data-redis")
 }
 
 tasks.withType<Test> {
