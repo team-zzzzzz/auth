2f592fae2211df4b32ee9956a14a5aeb3eddeda4
juno-choi <ililil9482@naver.com>
2024-05-18 10:30:53 +0900

    feat : querydsl 설정 완료

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,10 +1,7 @@
-import com.ewerk.gradle.plugins.tasks.QuerydslCompile
-
 plugins {
 	java
 	id("org.springframework.boot") version "3.3.0-RC1"
 	id("io.spring.dependency-management") version "1.1.4"
-	id("com.ewerk.gradle.plugins.querydsl") version "1.0.10"
 }
 
 group = "com.team5z"
@@ -47,28 +44,13 @@ dependencies {
 	runtimeOnly("io.jsonwebtoken:jjwt-impl:0.11.2")
 	runtimeOnly("io.jsonwebtoken:jjwt-jackson:0.11.2")
 	implementation("org.springframework.boot:spring-boot-starter-data-redis")
+
 	implementation("com.querydsl:querydsl-jpa:${queryDslVersion}:jakarta")
 	annotationProcessor("com.querydsl:querydsl-apt:${queryDslVersion}:jakarta")
+	annotationProcessor("jakarta.annotation:jakarta.annotation-api")
+	annotationProcessor("jakarta.persistence:jakarta.persistence-api")
 }
 
 tasks.withType<Test> {
 	useJUnitPlatform()
-}
-
-val querydslDir = layout.buildDirectory.dir("generated/querydsl").get().asFile
-
-querydsl {
-	jpa = true
-	querydslSourcesDir = querydslDir.toString()
-}
-sourceSets.getByName("main") {
-	java.srcDir(querydslDir)
-}
-configurations {
-	named("querydsl") {
-		extendsFrom(configurations.compileClasspath.get())
-	}
-}
-tasks.withType<QuerydslCompile> {
-	options.annotationProcessorPath = configurations.querydsl.get()
 }
--- src/test/java/com/team5z/projectAuth/auth/service/MemberServiceTest.java
+++ src/test/java/com/team5z/projectAuth/auth/service/MemberServiceTest.java
@@ -1,9 +1,13 @@
 package com.team5z.projectAuth.auth.service;
 
+import com.team5z.projectAuth.auth.domain.dto.MemberRequest;
 import com.team5z.projectAuth.auth.domain.entity.MemberEntity;
+import com.team5z.projectAuth.auth.repository.MemberRepository;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -12,10 +16,35 @@ class MemberServiceTest {
     @Autowired
     private MemberService memberService;
 
+    @Autowired
+    private MemberRepository memberRepository;
+
+    @Autowired
+    private BCryptPasswordEncoder bCryptPasswordEncoder;
+
     @Test
     void getMember() {
+        //given
+        MemberRequest request = MemberRequest.builder()
+                .email("mail@mail.com")
+                .name("최준호")
+                .address("주소")
+                .tel("01012345678")
+                .addressDetail("상세주소")
+                .zipCode("1234")
+                .bizNumber("12345678")
+                .type("SELLER")
+                .password("123456")
+                .passwordCheck("123456")
+                .nickname("닉네임")
+                .build();
+        memberRepository.save(MemberEntity.from(request, bCryptPasswordEncoder));
+
+        //when
         MemberEntity member = memberService.getMember();
-        System.out.println("test");
+
+        //then
+        Assertions.assertThat(member).isNotNull();
     }
 
 }
